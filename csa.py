a194d8a52990a35084dbdb9ff55b9d33c7a0588b
Bambang Setiadi <bambang@gmail.com>
2011-11-18 11:30:02 +0900

    add routine to read raw data directly to file.

--- csa.py
+++ csa.py
@@ -1,13 +1,9 @@
+#!/usr/bin/env python
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def readdata:
-    
-    return
-# extended chirp scaling
-# example raw data point target from file 'pointtarget.raw'
-# the file contains a simulated point target with Envisat ASAR
+# extended chirp scaling algorithm 
 # system parameters:
 c = float(299792458)  # speed of light in vacuum
 f_c = float(5331004416)  # carrier frequency
@@ -30,4 +26,78 @@ f_a = np.arange(-PRF/2+f_dc,f_dc+PRF/2-PRF/echoes,PRF/echoes)
 f_r = np.arange(-f_s/2, f_s/2-f_s/samples, f_s/samples)
 lbd = c/f_c
 
-#
+# example raw data point target from file 'pointtarget.raw'
+# the file contains a simulated point target with Envisat ASAR
+# read data from file 
+filename = 'pointtarget.raw' 
+
+# desired region 
+firstEcho=1 
+numEchoes=2206        
+
+f = file(filename,'rb')   # read in binary mode, must do on windows!
+data_type_int32 = np.dtype('int32').newbyteorder('B') # big endian 
+data_type_float32 = np.dtype('float32').newbyteorder('B') # big endian 
+data_type_complex64 = np.dtype('complex64').newbyteorder('B') # big endian 
+headerInts = np.fromfile(f, dtype=data_type_int32,count=8)
+
+#ras_magic=headerInts[0]
+ras_width=headerInts[1]
+#ras_height=headerInts[2]
+#ras_depth=headerInts[3]
+#ras_length=headerInts[4]
+#ras_type=headerInts[5]
+#ras_maptype=headerInts[6]
+#ras_maplength=headerInts[7]
+
+# skip to desired line 
+f.seek(data_type_complex64.itemsize*ras_width*(firstEcho-1),1)
+
+#read_count=0    
+echoData = np.fromfile(f, dtype=data_type_complex64, count=numEchoes*ras_width).reshape(numEchoes,ras_width)
+#print curEcho.size
+f.close()    
+
+# plot the raw echo data
+plt.figure(1)
+plt.subplot(121)
+plt.imshow(np.angle(echoData))
+plt.gray()
+plt.title('Raw Data (real)')
+
+# azimuth fft 
+data = np.fft.fftshift(np.fft.fft(np.fft.fftshift(echoData, axes=0),axis=0),axes=0)
+
+# display 
+plt.subplot(122)
+plt.title('After Azimuth FFT (real)')
+plt.imshow(np.angle(data))
+
+plt.show()
+
+# ========================================================================
+# chirp scaling, range scaling: H1
+# ========================================================================
+beta = (1 - (f_a*lbd/2/v)**2)**0.5
+a = 1/beta - 1
+R = r_ref/beta
+a_scl = a + ((1-alpha)*(1+a))/alpha
+k_r = -B/t_p
+
+# WARNING: in the work of Moreira et al, 1996 k_r is defined negative
+# because they assume a "down-chirp", whereas we assume an "up-chirp" and
+# must explicitely set k_r as negative for the following transfer functions
+# to be equal to the theoretical formulations.
+
+k_inv = 1/k_r - (2*lbd *r_ref *(beta**2-1))  /(c**2* (beta**3))
+k = 1/k_inv
+
+x = k*a_scl
+bigx = x*np.ones((1,samples))
+Tau = np.ones(echoes,1)*tau
+y = 2.*R./c
+Y = y(:)*ones(1,samples);
+Z = (Tau-Y).^2;
+H1 = exp(-j*pi*X.*Z);
+
+data = data .* H1;
